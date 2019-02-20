f023c934b04a5c23cf67ff41ec90ef3bdb89d72c
Ronderson de Almeida Florentino <ronderson00@gmail.com>
2019-02-20 09:14:46 -0400

    alterando o listar Usuarios

--- views/listUsuarios.php
+++ views/listUsuarios.php
@@ -1,8 +1,7 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
-  <a class="navbar-brand" href="#">Sistema Loja</a>  
+    <a class="navbar-brand" href="#">Sistema Loja</a>  
 </nav>
 <main role="main" >
-<<<<<<< HEAD
     <div class="container"> 
         <div class="row">
             <div class="col-md-8 offset-md-2"> 
@@ -37,7 +36,7 @@
                             </div>
                         </div>
                         <a class="btn btn-success" href="<?php echo BASE_URL ?>home/cadastro">+ Usuario</a>
-                    </div>       
+                    </div>
                     <table class="table table-striped mt-3">
                         <thead>        
                             <tr>            
@@ -48,13 +47,13 @@
                         </thead>
                         <tbody>
                             <?php if (isset($usuarios) && !empty($usuarios)): ?>
-                                <?php foreach ($usuarios as $user): ?>                                        
+                                <?php foreach ($usuarios as $user): ?>
                                     <tr>            
                                         <td><?php echo $user['nome']; ?></td>
                                         <td><?php echo $user['perfil']; ?></td>
                                         <td align="center">                
                                             <a href="<?php echo BASE_URL ?>home/editCadastro/<?php echo $user['id'] ?>" class="btn btn-warning btn-sm">Editar</a>
-                                            <button  onclick="del(<?php echo $user['id'];?>)" class="btn btn-danger btn-sm">Excluir</button>
+                                            <button  onclick="del(<?php echo $user['id'] ?>)" class="btn btn-danger btn-sm">Excluir</button>
                                         </td>
                                     </tr>                
                                 <?php endforeach; ?>
@@ -65,89 +64,27 @@
             </div>
         </div>
     </div>
-=======
-<div class="container"> 
-  <div class="row">
-    <div class="col-md-8 offset-md-2"> 
-    <h1>Listar Usuario  </h1>
-    <div class="text-right m-2">   
-      <div class="btn-group ">
-      <div class="dropdown mr-1">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Visualizar PDF
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-    <a class="dropdown-item" href="<?php echo BASE_URL?>home/gerarPdfPerfil" target="_blank">Usuarios por Perfil</a>
-    <a class="dropdown-item" href="<?php echo BASE_URL?>home/gerarPdfAparelho" target="_blank">Usuarios por Aparelho</a>    
-  </div>
-</div>
-<div class="dropdown mr-1">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Exportar CSV
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-    <a class="dropdown-item" href="<?php echo BASE_URL?>home/gerarCsvPerfil" target="_blank">Usuarios por Perfil</a>
-    <a class="dropdown-item" href="<?php echo BASE_URL?>home/gerarCsvAparelho" target="_blank">Usuarios por Aparelho</a>    
-  </div>
-</div>
-<div class="dropdown mr-1">
-  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Exportar TXT
-  </button>
-  <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-    <a class="dropdown-item" href="<?php echo BASE_URL?>home/gerarTxtPerfil" target="_blank">Usuarios por Perfil</a>
-    <a class="dropdown-item" href="<?php echo BASE_URL?>home/gerarTxtAparelho" target="_blank">Usuarios por Aparelho</a>    
-  </div>
-</div>
-  <a class="btn btn-success" href="<?php echo BASE_URL?>home/cadastro">+ Usuario</a>
-</div>
-        <table class="table table-striped mt-3">
-        <thead>        
-            <tr>            
-            <th scope="col">Nome</th>
-            <th scope="col">Perfil</th>
-            <th scope="col" class="text-center">Ações</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php if(isset($usuarios) && !empty($usuarios)):?>
-        <?php foreach($usuarios as $user):?>
-        <tr>            
-            <td><?php echo $user['nome'];?></td>
-            <td><?php echo $user['perfil'];?></td>
-            <td align="center">                
-                <a href="<?php echo BASE_URL?>home/editCadastro/<?php echo $user['id']?>" class="btn btn-warning btn-sm">Editar</a>
-                <button  onclick="del(<?php echo $user['id']?>)" class="btn btn-danger btn-sm">Excluir</button>
-            </td>
-            </tr>                
-        <?php endforeach;?>
-        <?php endif;?>              
-        </tbody>
-        </table>
-   </div>    
-  </div>
-</div>
->>>>>>> 19cfb3d916131165646c1d171b8cc8bb3442cd82
+
 </main>
 <script>
-    function del(id){
-        
-    swal({
-        title: "Deseja Excluir este Usuario?",      
-        icon: "warning",
-         buttons: ["NÃO","SIM"],
-        dangerMode: true,
-        closeOnClickOutside: false,
-        closeOnEsc: false,
-      })
-      .then((willDelete) => {        
-        if (willDelete) {            
-          swal("Usuario Excluido Com Sucesso", {
-            icon: "success",            
-          }).then(()=>{
-            location.href="<?php echo BASE_URL?>home/delUsuario/"+ id;
-          });          
-        }        
-      });
-    }
+            function del(id){
+
+            swal({
+            title: "Deseja Excluir este Usuario?",
+                    icon: "warning",
+                    buttons: ["NÃO", "SIM"],
+                    dangerMode: true,
+                    closeOnClickOutside: false,
+                    closeOnEsc: false,
+            })
+                    .then((willDelete) = > {
+                    if (willDelete) {
+                    swal("Usuario Excluido Com Sucesso", {
+                    icon: "success",
+                    }).then(() = > {
+                    location.href = "<?php echo BASE_URL ?>home/delUsuario/" + id;
+                    });
+                    }
+                    });
+            }
 </script>
